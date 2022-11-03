6a1aa014ebfaf6b7df1d4073939c28beeafbd536
Hamid Tavakoli <hamid@topcoder.com>
2022-11-03 01:15:36 -0700

    WIP

--- src/handlers/handler.ts
+++ src/handlers/handler.ts
@@ -1,36 +1,13 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda"
-import { ParquetReader } from 'parquets'
-import fs from 'fs'
 import { getPqFiles } from "src/resources/s3FileOpener"
 
 const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
-  await getPqFiles()
-  // const names = fs.readdirSync('./src/pqFiles')
-  // const data = await Promise.all(names.map(async (name: string) => {
-  //   const tempData = [];
-  //   const reader = await ParquetReader.openFile('./src/pqFiles/' + name)
-  //   const cursor = reader.getCursor()
-  //   let record = await cursor.next();
-  //   while (record) {
-  //     tempData.push(record)
-  //     record = await cursor.next();
-  //   }
-  //   return tempData
+  const { Bucket, Path, MaxSize } = event.body ? JSON.parse(event.body) : null
+  if (!Bucket || !Path || !MaxSize) throw new Error('Missing required parameters')
+  console.log({ Bucket, Path, MaxSize })
+  //Convert max size to bytes
+  await getPqFiles(Bucket, Path, MaxSize * 1024 * 1024)
 
-  // }))
-  // console.log(data)
-  // console.log(`Data is :`, JSON.stringify(data, null, 4))
-  // console.log(s3data)
-  // const reader = await ParquetReader.openBuffer(s3data)
-  // const cursor = reader.getCursor()
-  // let record = await cursor.next();
-  // const tempData = [];
-  // while (record) {
-  //   console.log(record)
-  //   tempData.push(record)
-  //   record = await cursor.next();
-  // }
-  // console.log(tempData)
   return {
     statusCode: 200,
     body: 'done',
--- src/resources/s3FileOpener.ts
+++ src/resources/s3FileOpener.ts
@@ -1,34 +1,95 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { S3Client, ListObjectsCommand, GetObjectCommand, S3 } from '@aws-sdk/client-s3';
+import { S3Client, ListObjectsCommand, GetObjectCommand, ListObjectsCommandOutput } from '@aws-sdk/client-s3';
 import type { Readable } from 'stream'
 import { ParquetReader } from 'parquets'
 import { AWS_REGION } from 'src/conf';
 
+interface S3FileList {
+  Key: string;
+  Size: number;
+  ETag: string;
+  StorageClass: string;
+}
 const client = new S3Client({
   region: AWS_REGION,
+  //TODO:Remove this
+  credentials: {
+    accessKeyId: <string>process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: <string>process.env.AWS_SECRET_ACCESS_KEY,
+    sessionToken: process.env.AWS_SESSION_TOKEN,
+  }
 });
 
-export const getPqFiles = async (): Promise<any> => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const getPqFiles = async (Bucket: string, Prefix: string, MaxSize: number): Promise<any> => {
   const params = {
-    Bucket: 'tc-dw-dev-dw-raw',
-    Prefix: 'Submission/submission/2019/10/10/'
+    Bucket,
+    Prefix
   };
   const ListCommand = new ListObjectsCommand(params);
-  const data = await client.send(ListCommand)
-  console.log(data.Contents ? data.Contents[0] : 'No data')
-  const fileName = data.Contents ? data.Contents[0].Key : 'No data'
+  const data: ListObjectsCommandOutput = await client.send(ListCommand)
+  const objectList = <S3FileList[]>data.Contents
+  if (!objectList) throw new Error('No objects found')
 
-  // eslint-disable-next-line no-async-promise-executor
-  const data2: Buffer[] = await new Promise(async (resolve, reject) => {
-    const getObjectCommand = new GetObjectCommand({
-      Bucket: 'tc-dw-dev-dw-raw',
-      Key: fileName
-    });
+  let size = 0;
+  let i = 0;
+  const files: { Key: string, index: number }[] = [];
+  objectList.forEach((item: S3FileList) => {
+    size += item.Size ? item.Size : 0;
+    if (size > MaxSize) {
+      size = item.Size;
+      i++;
+    }
+    files.push({ Key: item.Key, index: i })
+
+  })
+
+  const sortedFiles: { Key: string }[][] = []; //Array of arrays of files
+  files.forEach((item: { Key: string, index: number }) => {
+    if (!sortedFiles[item.index]) sortedFiles[item.index] = [];
+    sortedFiles[item.index].push({ Key: item.Key })
+  })
+
+  const fileContent: Buffer[][] = []
+  await sortedFiles.map(async (items: { Key: string }[], index: number) => {
+    await items.map(async (file: { Key: string }) => {
+      const content = await getFileContents(Bucket, file.Key)
+      if (!fileContent[index]) fileContent[index] = [];
+      fileContent[index].push(content)
+    })
+  })
+
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const pqContent: any = []
+
+  await fileContent.map(async (items: Buffer[], index: number) => {
+    await items.map(async (file: Buffer) => {
+      if (!pqContent[index]) pqContent[index] = [];
+      const reader = await ParquetReader.openBuffer(file)
+      const cursor = reader.getCursor()
+      let row = await cursor.next()
+      while (row) {
+        pqContent[index].push(row)
+        row = await cursor.next()
+      }
+    })
+  })
+
+
+}
+
+const getFileContents = async (Bucket: string, Key: string): Promise<Buffer> => {
+  const getObjectCommand = new GetObjectCommand({
+    Bucket,
+    Key
+  });
+  const response = await client.send(getObjectCommand)
+
+  // eslint-disable-next-line consistent-return
+  const data: Buffer = await new Promise((resolve: Function, reject: Function) => {
     try {
-      const response = await client.send(getObjectCommand)
-
       // Store all of data chunks returned from the response data stream 
-      // into an array then use Array#join() to use the returned contents as a String
+      // into an array then use Buffer.concat to use the returned contents as a Buffer
       const responseDataChunks: Buffer[] = [];
       // Handle an error while streaming the response body
       (response.Body as Readable).on('error', (err: Error) => reject(err));
@@ -37,51 +98,12 @@ export const getPqFiles = async (): Promise<any> => {
       // Each chunk is a Buffer instance
       (response.Body as Readable).on('data', (chunk: Buffer) => responseDataChunks.push(chunk));
 
-      // Once the stream has no more data, join the chunks into a string and return the string
-      (response.Body as Readable).on('end', () => resolve(responseDataChunks))
+      // Once the stream has no more data, join the chunks into a Buffer and return the Buffer
+      (response.Body as Readable).on('end', () => resolve(Buffer.concat(responseDataChunks)))
     } catch (err) {
       // Handle the error or throw
       return reject(err)
     }
   })
-
-  const reader = await ParquetReader.openBuffer(Buffer.concat(data2))
-  const cursor = reader.getCursor()
-  let record = await cursor.next();
-  const tempData = [];
-  while (record) {
-    console.log(record)
-    tempData.push(record)
-    record = await cursor.next();
-  }
-  return data2
-
-  // const getObjectCommand = new GetObjectCommand({
-  //   Bucket: 'tc-dw-dev-dw-raw',
-  //   Key: fileName
-  // });
-  // const data3: any = await client.send(getObjectCommand);
-  // // Convert the ReadableStream to a string.
-  // const val = await data3.body
-  // console.log(val)
-  // const reader = await ParquetReader.openBuffer(val)
-  // const cursor = reader.getCursor()
-  // let record = await cursor.next();
-  // const tempData = [];
-  // while (record) {
-  //   console.log(record)
-  //   tempData.push(record)
-  //   record = await cursor.next();
-  // }
-  // return data3.Body
-
-  //   const getObjectCommand = new GetObjectCommand({
-  //     Bucket: 'tc-dw-dev-dw-raw',
-  //     Key: fileName
-  //   });
-  // const file = await client.getObject({
-  //   Bucket: 'tc-dw-dev-dw-raw',
-  //   Key: fileName
-  // })
-  // return file;
+  return data
 }
